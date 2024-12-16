27ca2889d750ab361d68b49a8d263286a6c30e03
Michael Hackl <michael.hackl@tum.de>
2024-12-16 15:29:03 +0000

    work on wallet implementation

--- dockerfiles/wallet/python/server.py
+++ dockerfiles/wallet/python/server.py
@@ -25,21 +25,18 @@ def process_request():
     with wallet.Wallet() as w:
         print(f"trying to execute trustlet {int(os.environ['TRUSTLET'])} with {len(data)} output size.")
         trustlet = wallet.Trustlet(int(os.environ['TRUSTLET']))
-        output_len = 103000 # 102067 for 503
+        output_len = 103000 # 102067 for benchmark 503
         ret = trustlet.invoke_trustlet(data, output_len)
-        print(f"trustlet returned: {ret}") # todo: remove!
         ret = json.loads(ret)
 
     end = datetime.datetime.now()
-
-    # memory.close()
 
     return {
         "begin": begin.strftime("%s.%f"),
         "end": end.strftime("%s.%f"),
         "request_id": str(uuid.uuid4()),
         "is_cold": False,
-        "result": {"output": None}, # todo: temp
+        "result": {"output": ret},
     }
 
 
