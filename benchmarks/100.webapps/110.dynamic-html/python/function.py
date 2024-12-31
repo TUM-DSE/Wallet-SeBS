c9e998ffe33fc601d0b4cf3ac075bf40c09ee91c
Michael Hackl <michael.hackl@tum.de>
2024-12-31 11:24:15 +0000

    fix benchmarks requiring extra files

--- benchmarks/100.webapps/110.dynamic-html/python/function.py
+++ benchmarks/100.webapps/110.dynamic-html/python/function.py
@@ -6,8 +6,6 @@ import os
 
 from jinja2 import Template
 
-SCRIPT_DIR = path.abspath(path.join(path.dirname(__file__)))
-
 def handler(event):
 
     # start timing
@@ -15,7 +13,7 @@ def handler(event):
     size = event.get('random_len')
     cur_time = datetime.now()
     random_numbers = sample(range(0, 1000000), size)
-    template = Template( open(path.join(SCRIPT_DIR, 'templates', 'template.html'), 'r').read())
+    template = Template( open(path.join('python', 'template.html'), 'r').read())
     html = template.render(username = name, cur_time = cur_time, random_numbers = random_numbers)
     # end timing
     # dump stats 
--- benchmarks/400.inference/411.image-recognition/python/function.py
+++ benchmarks/400.inference/411.image-recognition/python/function.py
@@ -18,8 +18,7 @@ from torchvision.models import resnet50
 from . import storage
 client = storage.storage.get_instance()
 
-SCRIPT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-class_idx = json.load(open(os.path.join(SCRIPT_DIR, "imagenet_class_index.json"), 'r'))
+class_idx = json.load(open(os.path.join("python", "imagenet_class_index.json"), 'r'))
 idx2label = [class_idx[str(k)][1] for k in range(len(class_idx))]
 model = None
 
