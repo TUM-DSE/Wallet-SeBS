bc71c7038bd6a3732170464bd5e249c27bf89be7
Michael Hackl <michael.hackl@tum.de>
2025-01-09 00:39:42 +0000

    change pagerank implementation

--- benchmarks/500.scientific/501.graph-pagerank/python/function.py
+++ benchmarks/500.scientific/501.graph-pagerank/python/function.py
@@ -10,7 +10,7 @@ def handler(event):
     graph_generating_end = datetime.datetime.now()
 
     process_begin = datetime.datetime.now()
-    result = graph.pagerank()
+    result = graph.pagerank(implementation="arpack")
     process_end = datetime.datetime.now()
 
     graph_generating_time = (graph_generating_end - graph_generating_begin) / datetime.timedelta(microseconds=1)
