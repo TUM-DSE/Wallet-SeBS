6403eb33a81065b217d159961df4637c09bbc18d
Michael Hackl <michael.hackl@tum.de>
2025-02-06 19:12:11 +0000

    do remaining benchmarks

--- benchmarks/200.multimedia/220.video-processing/python/server.py
+++ benchmarks/200.multimedia/220.video-processing/python/server.py
@@ -27,26 +27,12 @@ def process_request():
     input_prefix = data.get('bucket').get('input')
     output_prefix = data.get('bucket').get('output')
     key = data.get('object').get('key')
-    download_path = '/tmp/{}-{}'.format(key, uuid.uuid4())
-    os.makedirs(download_path)
 
-    s3_download_begin = datetime.datetime.now()
-    client.download_directory(bucket, os.path.join(input_prefix, key), download_path)
-    s3_download_stop = datetime.datetime.now()
+    download_begin = datetime.datetime.now()
+    file = client.download_stream(bucket, os.path.join(input_prefix, key))
+    download_stop = datetime.datetime.now()
 
-    data['files'] = {}
-    for dirpath, dirnames, filenames in os.walk(download_path):
-        relative_path = os.path.relpath(dirpath, download_path)
-        current_level = data['files']
-
-        if relative_path != ".":
-            for part in relative_path.split(os.sep):
-                current_level = current_level.setdefault(part, {})
-
-        for filename in filenames:
-            file_path = os.path.join(dirpath, filename)
-            with open(file_path, "rb") as f:
-                current_level[filename] = f.read()
+    data['file'] = file
 
     data = pickle.dumps(data)
 
@@ -56,7 +42,7 @@ def process_request():
     with wallet.Wallet() as w:
         print(f"trying to execute trustlet {int(os.environ['TRUSTLET'])} with {len(data)} output size.")
         trustlet = wallet.Trustlet(int(os.environ['TRUSTLET']))
-        output_len = 9328000 # 9327022 -> 9328000 for benchmark 311
+        output_len = 9328000 # ?????? for benchmark 220
         trustlet.invoke_trustlet_bin("", 0)
         begin = datetime.datetime.now()
         trustlet.invoke_trustlet_bin(data, 0)
@@ -64,19 +50,20 @@ def process_request():
         ret = trustlet.invoke_trustlet_bin("", output_len)
         ret = pickle.loads(ret)
 
-    s3_upload_begin = datetime.datetime.now()
-    archive_name = '{}.zip'.format(key)
-    key_name = client.upload_stream(bucket, os.path.join(output_prefix, archive_name), io.BytesIO(ret.get('result')))
-    s3_upload_stop = datetime.datetime.now()
+
+    upload_begin = datetime.datetime.now()
+    filename = os.path.basename(ret['result']['upload_path'])
+    upload_key = client.upload_stream(bucket, os.path.join(output_prefix, filename), io.BytesIO(ret['result']['data']))
+    upload_stop = datetime.datetime.now()
 
     ret['result'] = {
         'bucket': bucket,
-        'key': key_name
+        'key': upload_key
     }
 
     return {
         "begin": begin.strftime("%s.%f"),
-        "end": (end + (s3_download_stop - s3_download_begin) + (s3_upload_stop - s3_upload_begin)).strftime("%s.%f"),
+        "end": (end + (download_stop - download_begin) + (upload_stop - upload_begin)).strftime("%s.%f"),
         "request_id": str(uuid.uuid4()),
         "is_cold": False,
         "result": {"output": ret},
--- benchmarks/300.utilities/311.compression/python/function.py
+++ benchmarks/300.utilities/311.compression/python/function.py
@@ -1,10 +1,7 @@
 import datetime
-import io
 import os
 import shutil
 import uuid
-import base64
-import zlib
 
 def parse_directory(directory):
 
@@ -16,29 +13,37 @@ def parse_directory(directory):
 
 def handler(event):
 
+    key = event.get('object').get('key')
     download_path = '/tmp/{}-{}'.format(key, uuid.uuid4())
-    os.makedirs(download_path)
 
-    s3_download_begin = datetime.datetime.now()
-    client.download_directory(bucket, os.path.join(input_prefix, key), download_path)
-    s3_download_stop = datetime.datetime.now()
+    list = [(download_path, event.get('files'))]
+    while list:
+        current_path, current_level = list.pop()
+        os.makedirs(current_path, exist_ok=True)
+        for name, contents in current_level.items():
+            item_path = os.path.join(current_path, name)
+            if isinstance(contents, dict):
+                list.append((item_path, contents))
+            else:
+                with open(item_path, "wb") as f:
+                    f.write(contents)
+
     size = parse_directory(download_path)
 
     compress_begin = datetime.datetime.now()
     shutil.make_archive(os.path.join(download_path, key), 'zip', root_dir=download_path)
     compress_end = datetime.datetime.now()
 
-    s3_upload_begin = datetime.datetime.now()
     archive_name = '{}.zip'.format(key)
     archive_size = os.path.getsize(os.path.join(download_path, archive_name))
-    key_name = client.upload(bucket, os.path.join(output_prefix, archive_name), os.path.join(download_path, archive_name))
-    s3_upload_stop = datetime.datetime.now()
+    with open(os.path.join(download_path, archive_name), "rb") as f:
+        result = f.read()
 
     #download_time = (s3_download_stop - s3_download_begin) / datetime.timedelta(microseconds=1)
     #upload_time = (s3_upload_stop - s3_upload_begin) / datetime.timedelta(microseconds=1)
     process_time = (compress_end - compress_begin) / datetime.timedelta(microseconds=1)
     return {
-            'result': base64.b64encode(resized).decode('ascii'),
+            'result': result,
             'measurement': {
                 #'download_time': download_time,
                 'download_size': size,
